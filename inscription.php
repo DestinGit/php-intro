68601df94925a57773382e66ef2e3fcfc0e2fd6f
formation <toto@toto.com>
2017-07-25 09:13:14 +0200

    remplacer par les fichiers du prof

--- inscription.php
+++ inscription.php
@@ -3,30 +3,100 @@
 <html>
 
 <head>
-    <title>Page Title</title>
+    <title>Formulaire age</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1.0">
 </head>
 
 <body>
-    <form method="post" action="valid-form.php">
-        <fieldset>
-            <legend>Inscription</legend>
-            <label for="email">Email</label>
-            <div>
-                <input type="email" name="email" id="email" placeholder="Email">
-            </div>
-            <label for="mdp">Mode de passe</label>
-            <div>
-                <input type="password" name="mdp" id="mdp" placeholder="Mot de passe">
-            </div>
-            <label for="mdp-confirm">Confirmation Mot de Passe</label>
-            <div>
-                <input type="password" name="mdp-confirm" id="mdp-confirm" placeholder="Confirmation">
-            </div>
-        </fieldset>
-        <button type="submit">Incription</button>
-    </form>
+
+<?php
+
+var_dump($_POST);
+
+//Récupération des données
+$email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+$mdp = filter_input(INPUT_POST, "mdp", FILTER_SANITIZE_STRING);
+$mdpConfirm = filter_input(INPUT_POST, "mdp-confirme", FILTER_SANITIZE_STRING);
+$submit = filter_input(INPUT_POST, "submit", FILTER_DEFAULT);
+$competences = filter_input(INPUT_POST, "competences", FILTER_DEFAULT, FILTER_REQUIRE_ARRAY)??[];
+
+var_dump($competences);
+
+$erreurs = [];
+$message = "";
+$estSoumis = isset($submit);
+
+//Si le formulaire est posté
+if($estSoumis){
+    //Validation des données
+    if(! $email){
+        $erreurs[] = "L'adresse email est invalide";
+    }
+
+    if(empty($mdp)){
+        $erreurs[] =  "Le mot de passe est vide";
+    } elseif($mdp != $mdpConfirm){
+        $erreurs[] = "Le mot de passe et sa confirmation ne sont pas identiques";
+    }
+
+    //Traitement des données
+
+    if(count($erreurs) == 0){
+        $message = "Votre inscription est confirmée";
+
+        //TODO enregistrer l'inscription dans un fichier ou une base de données
+    }
+}
+
+
+?>
+
+<div>
+    <?=$message?>
+</div>
+
+<!-- Affichage des erreurs -->
+<?php if(count($erreurs)): ?>
+<div>
+    <ul>
+        <?php foreach ($erreurs as $item): ?>
+        <li><?= $item; ?></li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+<?php endif; ?>
+
+<?php if(! $estSoumis || count($erreurs)>0): ?>
+<form method="post" novalidate>
+
+    <label for="email">Votre Adresse email</label>
+    <input type="email" id="email" name="email" value="<?= $email ?>"><br>
+
+    <label for="mdp">Votre mot de passe</label>
+    <input type="password" id="mdp" name="mdp"><br>
+
+    <label for="mdp-confirme">Confirmation du mot de passe</label>
+    <input type="password" id="mdp-confirme" name="mdp-confirme"><br>
+
+    <div>
+        <h3>Vos compétences</h3>
+
+        <input type="checkbox" name="competences[]" value ="Java" id="java">
+        <label for="java">Java</label><br>
+
+        <input type="checkbox" name="competences[]" value ="PHP" id="php">
+        <label for="php">PHP</label><br>
+
+        <input type="checkbox" name="competences[]" value="Python" id="python">
+        <label for="python">Python</label><br>
+    </div>
+
+
+    <button type="submit" name="submit">Valider</button>
+</form>
+<?php endif; ?>
+
 </body>
 
 </html>
--- traitement-formulaire.php
+++ traitement-formulaire.php
@@ -1,20 +1,25 @@
 <?php
-if($_POST) {
-    $message = '';
-    // récupérer les données
-    $age = $_POST["age"] ?? null;
-//    $age = intval($age);
-    
-    // Valider les données
-    if($age == null) {
-        $message .= "Vous devez passer par le formulaire";
-    } elseif(!is_numeric($age)) {
-        $message = "Vous devez saisir un nombre";
-    }
-    
-    $valid = empty($message);
-    if($valid) {
-        $message = $age < 18 ? "Mineur":"Majeur";
-    }
-    echo $message;
+
+var_dump($_POST);
+
+//Récupération de l'âge
+$age = $_GET["age"]?? null;
+$message = "";
+
+//Validation des données
+if(! isset($age)){
+    $message = "Vous devez passer par la saisie du formulaire";
+} elseif (! is_numeric($age)){
+    $message = "Vous devez saisir un nombre";
 }
+
+$valid = empty($message);
+
+//Traitement des données
+if($valid){
+    $message = $age<18?"mineur":"majeur";
+}
+
+//Affichage 
+echo $message;
+
